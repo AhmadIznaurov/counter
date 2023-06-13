e4d407449bbf73a219b4fb3dfb685398e5dccf22
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-06-13 18:39:13 +0300

    first commit

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,7 +1,7 @@
 import { useState, } from 'react';
 
 export const useCounter = (initialValue) => {
-    const [count = 2, setCount] = useState(initialValue);
+    const [count, setCount] = useState(initialValue);
 
     const increment = () => {
         setCount(count + 3);
